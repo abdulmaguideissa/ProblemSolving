266d0a91752f48e1d363416c7fa2da8c0f9e1265
Abdulmaguid Eissa <abdulmaguid.eissa@gmail.com>
2019-07-29 20:54:15 +0200

    Create AddTwoNums.cpp

---
+++ AddTwoNums.cpp
@@ -0,0 +1,47 @@
+/* Add two integer numbers in the form of reversed linked list
+   and save the result into a reversed linked list as well.
+   Challenge from leetCode.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode * digit = new ListNode(-1);
+        ListNode * res   = digit;
+        ListNode * uhead = l1;
+        ListNode * dhead = l2;
+        int remainder    = 0;
+        
+        int uval = 0;
+        int dval = 0;
+        int sum  = 0; 
+        
+        while(NULL != uhead || NULL != dhead){
+            uval = (uhead != NULL) ? uhead->val : 0;
+            dval = (dhead != NULL) ? dhead->val : 0;
+            sum  = remainder + uval + dval;
+            
+            remainder   = sum / 10;
+            digit->next = new ListNode(sum % 10);
+            digit       = digit->next;
+            
+            if(NULL != uhead)
+                uhead = uhead->next;
+            if(NULL != dhead)
+                dhead = dhead->next;
+        }
+        if(remainder){
+            digit->next = new ListNode(remainder);
+        }
+        return res->next;
+    }
+};
